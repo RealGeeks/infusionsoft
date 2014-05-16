2858c0897ecd5ddfc9e6035f59efd5b16ad47476
Kevin McCarthy <me@kevinmccarthy.org>
2014-05-16 10:49:37 -1000

    add setup.py

--- infusionsoft/__init__.py
+++ infusionsoft/__init__.py
@@ -1,1 +1,3 @@
 from .client import Infusionsoft
+
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+import infusionsoft
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name="infusionsoft",
+    version=infusionsoft.__version__,
+    description="A client library for the InfusionSoft API",
+    long_description=long_description,
+    url='https://github.com/realgeeks/infusionsoft',
+    author='Kevin McCarthy',
+    author_email='me@kevinmccarthy.org',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='infusionsoft client api',
+    packages=find_packages(),
+    install_requires = ['requests']
+)
